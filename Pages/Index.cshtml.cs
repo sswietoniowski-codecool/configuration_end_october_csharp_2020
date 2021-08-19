2e8ba4eb895038efea160f95633622f676b79ab0
Sławomir Świętoniowski <slawomir.swietoniowski@gmail.com>
2021-08-19 16:40:45 +0200

    Readen configuration through section

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -5,21 +5,30 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
 
 namespace configuration.Pages
 {
     public class IndexModel : PageModel
     {
         private readonly ILogger<IndexModel> _logger;
+        private IConfiguration _configuration;
 
-        public IndexModel(ILogger<IndexModel> logger)
+        public bool EnableGreeting { get; set; }
+        public string GreetingMessage { get; set; }
+        public string GreetingColor { get; set; }
+
+        public IndexModel(ILogger<IndexModel> logger, IConfiguration configuration)
         {
+            _configuration = configuration;
             _logger = logger;
         }
 
         public void OnGet()
         {
-
+            EnableGreeting = _configuration.GetValue<bool>("Features:HomePage:EnableGreeting");
+            GreetingMessage = _configuration.GetValue<string>("Features:HomePage:GreetingMessage");
+            GreetingColor = _configuration.GetValue<string>("Features:HomePage:GreetingColor");
         }
     }
 }
