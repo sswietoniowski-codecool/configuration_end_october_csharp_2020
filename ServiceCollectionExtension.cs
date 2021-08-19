8fc5f81ae9408f35d5629e0055f9c15c643abb28
Sławomir Świętoniowski <slawomir.swietoniowski@gmail.com>
2021-08-19 16:57:30 +0200

    Accessed configuration at Startup

---
+++ ServiceCollectionExtension.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Threading.Channels;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace configuration
+{
+    public static class ServiceCollectionExtension
+    {
+        public static IServiceCollection AddMyService(this IServiceCollection services, IConfiguration configuration)
+        {
+            bool myServiceEnabled = configuration.GetValue<bool>("MyService:Enabled");
+
+            if (myServiceEnabled)
+            {
+                string myServiceUrl = configuration.GetValue<string>("MyService:Url");
+
+                // we can use DI to instantiate our service here
+            }
+            else
+            {
+                // service is not enabled
+            }
+
+            return services;
+        }
+    }
+}
--- Startup.cs
+++ Startup.cs
@@ -23,6 +23,7 @@ namespace configuration
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddRazorPages();
+            services.AddMyService(Configuration);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
